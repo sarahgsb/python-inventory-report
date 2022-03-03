c55319cc6d5510d83a2075fe87e2b5d73463ecfb
Juliana de Assis Fernandes <julidanes@gmail.com>
2022-03-03 20:27:56 -0300

    Resolve o requisito 8

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,24 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+import sys
+
+
 def main():
-    pass
+
+    if len(sys.argv) == 3:
+        path = sys.argv[1]
+        report_type = sys.argv[2]
+        if path.endswith('.csv'):
+            obj = InventoryRefactor(CsvImporter)
+            return print(obj.import_data(path, report_type), end="")
+        elif path.endswith('.json'):
+            obj = InventoryRefactor(JsonImporter)
+            return print(obj.import_data(path, report_type), end="")
+        elif path.endswith('.xml'):
+            obj = InventoryRefactor(XmlImporter)
+            return print(obj.import_data(path, report_type), end="")
+    else:
+        sys.stderr.write('Verifique os argumentos\n')
